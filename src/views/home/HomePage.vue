5ea28c4d03a0f75f9d8230c0cbe86681178b9708
chenyu <chenyu@iict.ac.cn>
2023-11-06 23:02:49 +0800

    delete mock

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -79,56 +79,13 @@ const player = shallowRef([]);
 const curPage = ref(1);
 const pageSize = 10;
 
-const getVedioListFn = async () => {
-  // const res = await getVedeioList(curPage.value, pageSize);
-  // contentList = [...contentList, ...res];
-  // for (let i = 0; i < 10; i++) {
-  //   if (i % 2 === 0) {
-  //     contentList.push({
-  //       id: 2,
-  //       isDeleted: 2,
-  //       createBy: null,
-  //       createTime: '2023-11-05T15:42:58.246',
-  //       updateBy: null,
-  //       updateTime: '2023-11-05T15:42:58.246',
-  //       title: '风景',
-  //       author: null,
-  //       keyWord: null,
-  //       uid: 4,
-  //       poster: 'https://p9-pc-sign.douyinpic.com/image-cut-tos-priv/c7c7ff0dbc715f4e91c196974aec2745~tplv-dy-resize-origshort-autoq-75:330.jpeg?biz_tag=pcweb_cover&from=3213915784&s=PackSourceEnum_DOUYIN_WEB_NEW_PAGE&sc=cover&se=false&x-expires=2014416000&x-signature=GS7dC1gBqFm0DqMip%2Ff1digSN24%3D',
-  //       content: '风景',
-  //       urlList: [
-  //         'http://s39y9jpm6.hd-bkt.clouddn.com/hnit-wlwsys/202311051542443409.mp4'
-  //       ],
-  //       articleKind: 'VIDEO'
-  //     });
-  //   } else {
-  //     contentList.push({
-  //       id: 5,
-  //       isDeleted: 2,
-  //       createBy: null,
-  //       createTime: '2023-11-05T17:46:00.077',
-  //       updateBy: null,
-  //       updateTime: '2023-11-05T17:46:00.077',
-  //       title: '自由',
-  //       author: null,
-  //       keyWord: null,
-  //       poster: 'https://p3-pc-sign.douyinpic.com/image-cut-tos-priv/f77d9a8e440f874714c63484a2ab5699~tplv-dy-resize-origshort-autoq-75:330.jpeg?biz_tag=pcweb_cover&from=3213915784&s=PackSourceEnum_DOUYIN_WEB_NEW_PAGE&sc=cover&se=false&x-expires=2014538400&x-signature=HH2JTtFfheGennGG0DGQf1XG%2BWg%3D',
-  //       uid: 4,
-  //       content: '向前走就对了',
-  //       urlList: [
-  //         'http://s39y9jpm6.hd-bkt.clouddn.com/hnit-wlwsys/202311051745577279.mp4'
-  //       ],
-  //       articleKind: 'VIDEO'
-  //     });
-  //   }
-  // }
-  const res = await getVedeioList(curPage.value, pageSize);
+const getVedioListFn = async (page) => {
+  const res = await getVedeioList(page, pageSize);
   contentList.value = [...contentList.value, ...res.result];
   console.log('contentList.value', contentList.value);
   // console.log('res', res);
 };
-getVedioListFn();
+getVedioListFn(curPage.value);
 
 const handleMounted = async (payload) => {
   player.value.push(payload.player);
@@ -142,8 +99,10 @@ const handleEvent = (log) => {
 };
 
 const loadData = async ($state) => {
-  // 书写add函数
   console.log('底部');
+  curPage.value = curPage.value + 1;
+  getVedioListFn(curPage.value);
+  // 书写add函数
   // calling the api
 };
 
