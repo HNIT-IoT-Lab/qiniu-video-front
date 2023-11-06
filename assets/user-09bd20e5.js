8a1d0ea93497be7e976d70e97ff684d6ce17342b
fengyuan-liang <75926969+fengyuan-liang@users.noreply.github.com>
2023-11-06 17:27:50 +0000

    Deploying to gh-pages from  @ ba388482dcec851e68326d212b46b5a0fac4ec57 🚀

---
+++ assets/user-09bd20e5.js
@@ -0,0 +1,1 @@
+import{Y as n,r as t}from"./index-980d5e43.js";const u=n("user-store",()=>{const o=t(""),s=t({});return{token:o,userInfo:s,setToken:e=>{o.value=e,console.log("userToken",e),localStorage.setItem("token",e)},delToken:()=>{o.value="",localStorage.removeItem("token")},setUserInfo:e=>{console.log("info",e),s.value=e,console.log("setOK")},delUserInfo:()=>{s.value=""}}});export{u};
